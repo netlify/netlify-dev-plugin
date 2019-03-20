15f51a9250249f228f804d5e6c56d5db6a66dc87
sw-yx <swyx@dontemail.me>
2019-03-19 23:10:21 -0700

    undo some overeagerness

--- src/functions-templates/ts/template-registry.js
+++ src/functions-templates/ts/template-registry.js
@@ -5,16 +5,6 @@
 // priority is optional - for controlling what shows first in CLI
 module.exports = [
   {
-    name: 'auth-fetch',
-    description: 'Authenticated Fetch: uses node-fetch and Netlify Identity to access APIs',
-    onComplete() {
-      console.log(`authenticated node-fetch function created from template!`)
-      console.log(
-        'REMINDER: Make sure to call this function with the Netlify Identity JWT. See https://netlify-gotrue-in-react.netlify.com/ for demo'
-      )
-    }
-  },
-  {
     name: 'hello-world',
     priority: 1,
     description: 'Basic Hello World function: shows async/await usage, and response formatting'
@@ -24,34 +14,6 @@ module.exports = [
     description: 'Fetch function: uses node-fetch to hit an external API without CORS issues',
     onComplete() {
       console.log(`node-fetch function created from template!`)
-    }
-  },
-  {
-    name: 'serverless-ssr',
-    description: 'Serverless SSR: dynamic serverside rendering via functions',
-    onComplete() {
-      console.log(`serverless-ssr function created from template!`)
-    }
-  },
-  {
-    name: 'set-cookie',
-    description: 'Set Cookie: set a cookie alongside your function',
-    onComplete() {
-      console.log(`set-cookie function created from template!`)
-    }
-  },
-  {
-    name: 'protected-function',
-    description: 'Protected Function: Function behind Netlify Identity',
-    onComplete() {
-      console.log(`protected-function function created from template!`)
-    }
-  },
-  {
-    name: 'fauna-crud',
-    description: 'CRUD function: using Fauna DB!',
-    onComplete() {
-      console.log(`fauna-crud function created from template!`)
     }
   }
 ]
