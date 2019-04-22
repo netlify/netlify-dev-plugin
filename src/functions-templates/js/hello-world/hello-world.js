8583c98a30c65da1277a093fda92e4ff218a29ab
sw-yx <swyx@dontemail.me>
2019-04-21 20:41:27 -0400

    make hello world template a bit better for demos

--- src/functions-templates/js/hello-world/hello-world.js
+++ src/functions-templates/js/hello-world/hello-world.js
@@ -1,11 +1,7 @@
-async function hello() {
-  return Promise.resolve("Hello, World");
-}
-
-exports.handler = async function(event, context) {
+exports.handler = async (event, context) => {
   try {
-    const body = await hello();
-    return { statusCode: 200, body };
+    const subject = event.queryStringParameters.name || "World";
+    return { statusCode: 200, body: `Hello ${subject}` };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
